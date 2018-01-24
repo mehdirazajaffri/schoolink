041139e6252c80ca83d732670247e6e36f21ec34
Mehdi <mehdi.raza@venturedive.com>
2018-01-25 02:40:51 +0500

    All Setup

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,20 @@
+<li class="{{ Request::is('cities*') ? 'active' : '' }}">
+    <a href="{!! route('cities.index') !!}"><i class="fa fa-edit"></i><span>Cities</span></a>
+</li>
+
+<li class="{{ Request::is('cities*') ? 'active' : '' }}">
+    <a href="{!! route('cities.index') !!}"><i class="fa fa-edit"></i><span>Cities</span></a>
+</li>
+
+<li class="{{ Request::is('cities*') ? 'active' : '' }}">
+    <a href="{!! route('cities.index') !!}"><i class="fa fa-edit"></i><span>Cities</span></a>
+</li>
+
+<li class="{{ Request::is('cities*') ? 'active' : '' }}">
+    <a href="{!! route('cities.index') !!}"><i class="fa fa-edit"></i><span>Cities</span></a>
+</li>
+
+<li class="{{ Request::is('cities*') ? 'active' : '' }}">
+    <a href="{!! route('cities.index') !!}"><i class="fa fa-edit"></i><span>Cities</span></a>
+</li>
+
